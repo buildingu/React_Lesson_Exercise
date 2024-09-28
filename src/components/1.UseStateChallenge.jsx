bfcc50fa7a0b0ed5504e6eaad912f1900ec802e3
David <davidbish2002@hotmail.com>
2024-09-28 15:42:44 -0230

    Initial Commit

---
+++ src/components/1.UseStateChallenge.jsx
@@ -0,0 +1,17 @@
+/**
+ * Challenge 1: useState
+ * 
+ * Description:
+ * Create a toggle button to show and hide the div containing the text `BOO!`.
+ */
+
+import { useState } from "react";
+
+export default function UseStateChallenge() {
+  return (
+    <main>
+      <h1>useState Challenge</h1>
+      <div>BOO!</div>
+    </main>
+  );
+}
